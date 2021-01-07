49806ee6063046cbc22b14785eb56f0eb140a0d6
ysfbz <41655586+ysfbz@users.noreply.github.com>
2021-01-07 21:09:42 +0300

    Update n11-with-undetected-chrome-driver.py

--- n11-with-undetected-chrome-driver.py
+++ n11-with-undetected-chrome-driver.py
@@ -2,17 +2,29 @@ import undetected_chromedriver as uc
 uc.install()
 import unittest
 import time 
-
-
 from selenium.webdriver import Chrome
-driver = Chrome()
-driver.get('http://www.n11.com')
-driver.find_element_by_class_name("btnHolder").click()
-time.sleep(1)
-driver.find_element_by_class_name("btnSignIn").click()
-time.sleep(0)
-driver.find_element_by_name("email").send_keys("yusufbuzrul@gmail.com")
-driver.find_element_by_name("password").send_keys("milan1905")
-time.sleep(1)
-driver.find_element_by_id("loginButton").click()
-time.sleep(60)
+
+class LoginN11(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = Chrome()
+        self.driver.get("http://www.n11.com")
+        self.driver.get_cookies()
+
+
+    def test_n11_login(self):
+        self.driver.find_element_by_class_name("btnHolder").click()
+        time.sleep(1)
+        self.driver.find_element_by_class_name("btnSignIn").click()
+        time.sleep(0)
+        self.driver.find_element_by_name("email").send_keys("sendyour@email.com")
+        self.driver.find_element_by_name("password").send_keys("yourpassword")
+        time.sleep(1)
+        self.driver.find_element_by_id("loginButton").click()
+        time.sleep(60)
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
